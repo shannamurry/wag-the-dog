a49213cb067253ec7065133114d885955f81544b
shannamurry <shanna.murry84@gmail.com>
2021-07-26 10:04:12 -0400

    seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Area.create([{ name: "MontCo" }, { name: "Center City" }, { name: "Chestnut Hill" }, { name: "South Philly" }, { name: "Mt. Airy" }])
+
+User.create([{ email: "nihilistichare@gmail.com", password: "password" }])
+
+Walker.create([{ first_name: "Shanna", last_name: "Murry", email: "nihilistichare@gmail.com", phone: "267-903-4025", city: "Lansdale", area_id: 1, user_id: 1 }])
