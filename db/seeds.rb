29061a32c31f39a45977e0e300a8fc7881bfabba
shannamurry <shanna.murry84@gmail.com>
2021-07-26 09:58:22 -0400

    seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Area.create([{ name: "MontCo" }, { name: "Center City" }, { name: "Chestnut Hill" }, { name: "South Philly" }, { name: "Mt. Airy" }])
